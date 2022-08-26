bc2ea305c011f2f831ba90a43756628885199a68
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-08-26 14:27:36 +0200

    Added test projekct

---
+++ BankTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Bankkonto_ASP;
+
+namespace BankTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WithdrawShouldWork()
+        {
+            // Arrange
+            // Bankkonto_ASP bankAccount = new Bankkonto_ASP(); FUNKAR INTE
+            
+
+            // Act
+
+            // Assert
+        }
+
+        public void DepositShouldWork()
+        { 
+            // Arrange
+            // Act
+            // Assert        
+        }
+
+        public void GetCashShouldWork()
+        {
+            // Arrange
+            // Act
+            // Assert        
+        }
+
+    }
+}
