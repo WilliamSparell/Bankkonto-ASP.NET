2056ec7a88a7994e576c4419043bb9e6ca02b4fa
William Spårell <williamatlasnova@gmail.com>
2022-08-26 14:15:30 +0200

    hej

---
+++ Bankkonto ASP.NET/Controllers/Bankkonto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Bankkonto_ASP.NET.Controllers
+{
+    public class Bankkonto
+    {
+        double cash = 0;
+
+        double Deposit(double amount)
+        {
+            return cash += amount;
+        }
+
+        double Withdraw(double amount)
+        {
+            return cash -= amount;
+        }
+
+        double GetCash()
+        {
+            return cash;
+        }
+    }
+}
